714b0be0bb9241e3190a7b9146332453267d179e
leeheeweon <ggoddii@kaoni.com>
2023-12-01 15:36:19 +0900

    조건문

---
+++ java-start/src/variable/ex/CondOpEx.java
@@ -0,0 +1,9 @@
+package variable.ex;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a > b ? a : b);
+    }
+}
