cc838c47ce49fb7f413a945ed90b97bda360da80
leeheeweon <ggoddii@kaoni.com>
2023-12-08 13:39:01 +0900

    class

---
+++ java-basic/src/class1/ClassStart2.java
@@ -0,0 +1,12 @@
+package class1;
+
+public class ClassStart2 {
+    public static void main(String[] args) {
+        String[] studentNames = {"학생1", "학생2"};
+        int[] studentAges = {15, 16};
+        int[] studentsGrade = {90, 80};
+
+        System.out.println("이름:" + studentNames[0] + " 나이:" + studentAges[0] + " 성적:" + studentsGrade[0]);
+        System.out.println("이름:" + studentNames[1] + " 나이:" + studentAges[1] + " 성적:" + studentsGrade[1]);
+    }
+}
