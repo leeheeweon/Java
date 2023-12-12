d4d00849a70f679e83aca69fa65ea3750890fd61
leeheeweon <ggoddii@kaoni.com>
2023-12-12 10:16:59 +0900

    constructor

---
+++ java-basic/src/construct/MemberConstruct.java
@@ -0,0 +1,17 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    public MemberConstruct(String name, int age) {
+        this(name, age, 50);
+    }
+
+    public MemberConstruct(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
