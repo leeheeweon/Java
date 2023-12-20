e00e0d2a96fb1b14109d5b2c6a2eaac2eae666cc
leeheeweon <ggoddii@kaoni.com>
2023-12-20 15:35:12 +0900

    super

---
+++ java-basic/src/extends1/super1/Super1Main.java
@@ -0,0 +1,10 @@
+package extends1.super1;
+
+import static2.ex.Car;
+
+public class Super1Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
