88aa2bf37ed1fe17756daba98fcf75107e8b117a
leeheeweon <ggoddii@kaoni.com>
2023-12-26 15:05:24 +0900

    interface

--- java-basic/src/poly/ex4/AbstractMain.java
+++ java-basic/src/poly/ex4/AbstractMain.java
@@ -1,4 +1,4 @@
-package poly.ex3;
+package poly.ex4;
 
 
 public class AbstractMain {
@@ -16,10 +16,21 @@ public class AbstractMain {
         soundAnimal(dog);
         soundAnimal(cat);
         soundAnimal(caw);
+
+        moveAnimal(dog);
+        moveAnimal(cat);
+        moveAnimal(caw);
     }
+
     private static void soundAnimal(AbstractAnimal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
+    }
+
+    private static void moveAnimal(AbstractAnimal animal) {
+        System.out.println("동물 이동 테스트 시작");
+        animal.move();
+        System.out.println("동물 이동 테스트 종료");
     }
 }
