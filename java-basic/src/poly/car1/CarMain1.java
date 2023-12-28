cecbc3da7ab22c6e8ad89def33a9e7e883728369
leeheeweon <ggoddii@kaoni.com>
2023-12-28 09:17:48 +0900

    다형성

---
+++ java-basic/src/poly/car1/CarMain1.java
@@ -0,0 +1,12 @@
+package poly.car1;
+
+public class CarMain1 {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        driver.setCar(new K3Car());
+        driver.driver();
+
+        driver.setCar(new Model3Car());
+        driver.driver();
+    }
+}
